1eaa06a81d3fb4a5e92c9291da04b42973f8400e
Luis Viñas <rmvigo95@gmail.com>
2021-03-18 17:59:16 -0400

    User page started

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         'dark': '#222529',
         'second': '#323943'
+      },
+      boxShadow: {
+        'plane': '10px 10px 18px #1a232f, -10px -10px 18px #242f3f;'
       }
     }
   },
